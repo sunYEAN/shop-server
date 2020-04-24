fe59dd4d5f43fd45d35b659b475e5db9770cb26b
yangheng <bigyangmaster@163.com>
2020-04-24 10:28:38 +0800

    shop-server thinkJs

--- src/admin/controller/gallery.js
+++ src/admin/controller/gallery.js
@@ -8,4 +8,17 @@ module.exports = class extends Base {
     const gallery = await this.model('gallery').where(query).find();
     this.success(gallery);
   }
+
+  async storeAction() {
+    const params = this.post();
+    let model = this.get('model');
+
+    if (params.id) {
+      let id = params.id;
+      delete params.id;
+      model.where({id}).update(params);
+    } else model('gallery').add(params);
+
+    this.success(params);
+  }
 };
