89ec4688f478256667b5ea3771606f5addeb1a05
yangheng <bigyangmaster@163.com>
2020-05-17 23:25:30 +0800

    a

--- src/admin/controller/category.js
+++ src/admin/controller/category.js
@@ -7,7 +7,7 @@ module.exports = class extends Base {
    */
   async indexAction() {
     const model = this.model('category');
-    const data = await model.where({is_delete: 0}).order(['sort_order ASC']).select();
+    const data = await model.order(['sort_order ASC']).select();
     const topCategory = data.filter((item) => {
       return item.parent_id === 0;
     });
